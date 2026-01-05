2799c6d7e44b506f9d3b7a84b33235f1694bc415
dailenv-graphic <dailen.vazquez@oneagency.com>
2026-01-05 15:43:14 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+
+// remember to use module.exports instead of tailwind.config in production
+tailwind.config = 
+   {
+      // Note: config only includes the used styles & variables of your selection
+      content: ["./src/**/*.{html,vue,svelte,js,ts,jsx,tsx}"],
+      theme: {
+        extend: {
+          fontFamily: {
+            'font-family-font-1': "-, sans-serif",
+          },
+          fontSize: {
+            'font-size-16': "16px",
+          },
+          fontWeight: {
+            'font-weight-400': "undefined",
+          },
+          lineHeight: {
+            'line-height-26': "26px", 
+          },
+          letterSpacing: {
+             
+          },
+          borderRadius: {
+              
+          },
+          colors: {
+            
+            'branding-charcoal-500': '#2f333d',
+'color-blue-16': '#1a1f36',
+'neutral-20': '#ffffff',
+'primary-500': '#373299',
+'neutral-white': '#ffffff',
+'cta-main': '#ef2d61',
+          },
+          spacing: {
+              
+          },
+          width: {
+             
+          },
+          minWidth: {
+             
+          },
+          maxWidth: {
+             
+          },
+          height: {
+             
+          },
+          minHeight: {
+             
+          },
+          maxHeight: {
+             
+          }
+        }
+      }
+    }
+
+          
